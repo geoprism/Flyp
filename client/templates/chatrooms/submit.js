4a25bdf1a0728aed67bd9085e4cf464367d6b534
Brian Wong <wongbm2@uci.edu>
2016-04-23 15:53:20 -0700

    fixed empty input

--- client/templates/chatrooms/submit.js
+++ client/templates/chatrooms/submit.js
@@ -1,33 +1,37 @@
 Template.submit.events({
   'submit form':function(e){
     e.preventDefault();
-    if(Chats.find({roomid: this._id}).count() === 0){
-      var chat = {
-        content: $(e.target).find('[name=content]').val(),
-        author: Meteor.user().emails[0].address,
-        timestamp: new Date(),
-        roomid: this._id
-      };
+
+    if($(e.target).find('[name=content]').val() != ""){
+        if(Chats.find({roomid: this._id}).count() === 0){
+          var chat = {
+            content: $(e.target).find('[name=content]').val(),
+            author: Meteor.user().emails[0].address,
+            timestamp: new Date(),
+            roomid: this._id
+          };
+        }
+        else if(Chats.findOne({roomid: this._id}, {sort: {timestamp: -1}}).author === Meteor.user().emails[0].address){
+          var chat = {
+            content: $(e.target).find('[name=content]').val(),
+            author: Meteor.user().emails[0].address,
+            timestamp: new Date(),
+            roomid: this._id
+          };
+        }
+        else{
+          var chat = {
+            content: $(e.target).find('[name=content]').val(),
+            author: Meteor.user().emails[0].address,
+            timestamp: new Date(),
+            roomid: this._id
+          };
+          var chatroom = Chatrooms.findOne({_id: this._id});
+          var exchanges_num = chatroom.exchanges + 2;
+          Chatrooms.update({_id: this._id}, {$set:{exchanges: exchanges_num}});
+        }
     }
-    else if(Chats.findOne({roomid: this._id}, {sort: {timestamp: -1}}).author === Meteor.user().emails[0].address){
-      var chat = {
-        content: $(e.target).find('[name=content]').val(),
-        author: Meteor.user().emails[0].address,
-        timestamp: new Date(),
-        roomid: this._id
-      };
-    }
-    else{
-      var chat = {
-        content: $(e.target).find('[name=content]').val(),
-        author: Meteor.user().emails[0].address,
-        timestamp: new Date(),
-        roomid: this._id
-      };
-      var chatroom = Chatrooms.findOne({_id: this._id});
-      var exchanges_num = chatroom.exchanges + 2;
-      Chatrooms.update({_id: this._id}, {$set:{exchanges: exchanges_num}});
-    }
+
     console.log(Chatrooms.findOne({_id: this._id}).exchanges);
     chat._id = Chats.insert(chat);
     $('html, body').scrollTop( $(document).height() );
