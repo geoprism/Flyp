c0982ce9c7f4e1ddc91f5a7cabbe0068e3ac18a3
Jason <silentspeedz@gmail.com>
2016-04-23 04:29:26 -0700

    Fixed click errors

--- client/templates/mainpage/mainpage.js
+++ client/templates/mainpage/mainpage.js
@@ -6,23 +6,3 @@ Template.mainpage.helpers({
     return Chatrooms.find()
   }
 });
-
-Template.mainpage.events({
-  'click': function(e){
-    e.preventDefault();
-    if(Meteor.users.find().count() > 1){
-      var second_user = Random.choice(Meteor.users.find().fetch());
-      while(second_user._id === Meteor.user()._id)
-        second_user = Random.choice(Meteor.users.find().fetch());
-      var chatroom = {
-        author: Meteor.user()._id,
-        receiver: second_user._id,
-        title:'test'
-      };
-      chatroom._id = Chatrooms.insert(chatroom);
-      Router.go('chatrooms', chatroom);
-    }
-    else
-      alert("Not enough users! Please wait for more users to be registered");
-  }
-})
