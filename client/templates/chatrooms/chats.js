9bc5d7a57b4db8249076dcbcb25ac347c6b4c9f8
Brian Wong <wongbm2@uci.edu>
2016-04-23 01:50:45 -0700

    fixed the time feature

--- client/templates/chatrooms/chats.js
+++ client/templates/chatrooms/chats.js
@@ -1,5 +1,8 @@
 Template.chats.helpers({
   currentUser: function(){
     return Meteor.user().emails[0].address
+},
+  time: function(){
+      return moment(this.timestamp).format('h:mm a');
   }
 });
--- client/templates/chatrooms/submit.js
+++ client/templates/chatrooms/submit.js
@@ -5,8 +5,11 @@ Template.submit.events({
     var chat = {
       content: $(e.target).find('[name=content]').val(),
       //roomid:
-      author: Meteor.user().emails[0].address
+      author: Meteor.user().emails[0].address,
+      timestamp: new Date()
+
     };
+
 
     chat._id = Chats.insert(chat);
   }
