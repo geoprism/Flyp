31ce114daf498c96b215e7db34b71b426d1c6628
Brian Wong <wongbm2@uci.edu>
2016-04-23 04:42:56 -0700

    fixed cached msg

--- client/templates/chatrooms/submit.js
+++ client/templates/chatrooms/submit.js
@@ -2,11 +2,17 @@ Template.submit.events({
   'submit form':function(e){
     e.preventDefault();
 
+    $(document).ready(function () {
+    $('input[type=text], input[type=password]').attr('autocomplete', 'off');
+    });
+
     var chat = {
       content: $(e.target).find('[name=content]').val(),
       author: Meteor.user().emails[0].address,
       timestamp: new Date()
     };
+
+
 
     chat._id = Chats.insert(chat);
     $('html, body').scrollTop( $(document).height() );
