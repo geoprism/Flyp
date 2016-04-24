8843c89ff3db52bc0e8ce9f04e28a468f2f7e548
Brian Wong <wongbm2@uci.edu>
2016-04-24 04:35:28 -0700

    adding edits to main page

--- client/templates/mainpage/mainpage.js
+++ client/templates/mainpage/mainpage.js
@@ -4,5 +4,15 @@ Template.mainpage.helpers({
   },
   chatrooms_list: function(){
     return Chatrooms.find()
-  }
+},
+
+person: function(){
+    return Meteor.user().name;
+},
+
+school: function(){
+    return Meteor.user().college;
+}
+
+
 });
