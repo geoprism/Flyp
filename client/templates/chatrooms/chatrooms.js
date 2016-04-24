4fa380b094243223520cc1126714cebf5c05bc80
Jason <silentspeedz@gmail.com>
2016-04-23 17:02:27 -0700

    Added flip functionality

--- client/templates/chatrooms/chatrooms.js
+++ client/templates/chatrooms/chatrooms.js
@@ -1,8 +1,17 @@
 Template.chatrooms.helpers({
   chatlist: function(){
     return Chats.find({roomid : this._id});
+  },
+  roomname: function(){
+    if(this.author === Meteor.user()._id){
+      return Meteor.users.findOne(this.receiver).emails[0].address;
+    }
+    else {
+      return Meteor.users.findOne(this.author).emails[0].address;
+    }
+  },
+
+  flypped: function(){
+    return this.exchanges >= 5;
   }
-
-
-  
 });
