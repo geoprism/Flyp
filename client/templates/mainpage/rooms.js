5b1b1787c70b2f43661d4d46cd2dfc752a1a5eba
Jason <silentspeedz@gmail.com>
2016-04-24 00:10:21 -0700

    Added names instead of emails

--- client/templates/mainpage/rooms.js
+++ client/templates/mainpage/rooms.js
@@ -2,13 +2,13 @@ Template.rooms.helpers({
   partner: function(){
     if(this.receiver == Meteor.user()._id)
       if(this.exchanges >=5 )
-        return Meteor.users.findOne(this.author).emails[0].address;
+        return Meteor.users.findOne(this.author).name;
       else {
         return 'Classmate';
       }
     else
       if(this.exchanges >=5 )
-        return Meteor.users.findOne(this.receiver).emails[0].address;
+        return Meteor.users.findOne(this.receiver).name;
       else {
         return 'Classmate';
       }
